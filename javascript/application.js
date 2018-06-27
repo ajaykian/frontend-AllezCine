a04bb743a95f1452f0fc409f93c0629af3eb5d37
Colombe <c.schh@outlook.com>
2018-06-27 22:16:19 +0200

    scrolltop button + deleting horizontal scrollbar

--- javascript/application.js
+++ javascript/application.js
@@ -1,6 +1,6 @@
 function okCookie() {
     document.getElementById('cookieID').style.display = "none";
-    }
+}
 
 function genderChoice(className) {
     var $feature_movies = $('.featured-movies')
@@ -12,23 +12,13 @@ function genderChoice(className) {
     }
 }
 
-$('.modal-adult').modal({
-    backdrop: 'static',
-    keyboard: false
-})
-
-$('.modal-trailer').on('hidden.bs.modal', function () {
-    var $iframe = $(this).find('iframe');
-    $iframe.attr("src", $iframe.attr("src"));
-})
-
 function showFilms() {
     var moreFilms = document.getElementById("show-films-div");
     togglePosters(moreFilms);
 
     var moreToLess = document.getElementById("moreFilmsButton");
     console.log(moreToLess.innerHTML)
-    toggleButtonText(moreToLess);
+    toggleButtonText(moreToLess, "films");
 }
 
 function showSeries() {
@@ -37,7 +27,7 @@ function showSeries() {
 
     var moreToLess = document.getElementById("moreSeriesButton");
     console.log(moreToLess.innerHTML)
-    toggleButtonText(moreToLess);
+    toggleButtonText(moreToLess, "series");
 }
 
 function togglePosters(element) {
@@ -48,18 +38,18 @@ function togglePosters(element) {
     }
 }
 
-function toggleButtonText(button) {
-    if (button.innerHTML == "More films"){ 
-        button.innerHTML = "Less films";
+function toggleButtonText(button, label) {
+    if (button.innerHTML == "More " + label){ 
+        button.innerHTML = "Less " + label;
     } else {
-        button.innerHTML = "More films";
+        button.innerHTML = "More " + label;
     }
 }
 
-$('.modal-adult').modal({
-    backdrop: 'static',
-    keyboard: false
-}) 
+// $('.modal-adult').modal({
+//     backdrop: 'static',
+//     keyboard: false
+// }) 
 
 $('.modal-trailer').on('hidden.bs.modal', function () {
     var $iframe = $(this).find('iframe');
@@ -76,5 +66,18 @@ alert("First name : "+firstName+ "\nLast name : "+lastName + "\nEmail : " + emai
 
 });
 
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("backToTheTopButton").style.display = "block";
+    } else {
+        document.getElementById("backToTheTopButton").style.display = "none";
+    }
+}
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
